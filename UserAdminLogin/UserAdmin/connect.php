df9f496b176573b6e92fd450958f0675a0d7b645
Vikas Lokhande <90000685+vishudev777@users.noreply.github.com>
2024-02-28 10:33:58 +0530

    Delete UserAdminLogin/UserAdmin directory

--- UserAdminLogin/UserAdmin/connect.php
+++
@@ -1,8 +0,0 @@
-<?php 
-// created object ($con) of class mysqli  
-$con = new mysqli('localhost','root','','user_db'); // user_db is the Database Name
-if(!$con){
-    die(mysqli_error($con));
-}
-
-?>
